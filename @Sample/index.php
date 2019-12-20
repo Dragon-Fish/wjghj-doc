dc344cc60ad569b944eddcdc79e11040e09aa1d4
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2019-12-20 15:52:25 +0800

    Create index.php

---
+++ @Sample/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+<!-- Global head -->
+<?php require_once("/data/wwwroot/doc/settings.php"); ?>
+<?php require(globalinvoke."/head.php"); ?>
+<!-- End Global head -->
+</head>
+<body>
+<!-- Nav header & Side nav -->
+<?php require(globalinvoke."/nav.php"); ?>
+<?php require(globalinvoke."/sidenav.php"); ?>
+<!-- End Nav -->
+<!-- Content here -->
+<main>
+<div class="container row section">
+
+  <h2>Title</h2>
+  <p>content</p>
+
+</div>
+</main>
+<!-- End Content -->
+<!-- Footer -->
+<?php require(globalinvoke."/footer.php"); ?>
+<!-- End Footer -->
+</body>
+</html>
